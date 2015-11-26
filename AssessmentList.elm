d56076117c6f8d16dfc32ccd89ab17c4182d959f
Michael Wisely <michaelwisely@gmail.com>
2015-11-25 20:54:46 -0600

    Move init to Main.elm

--- AssessmentList.elm
+++ AssessmentList.elm
@@ -13,27 +13,6 @@ import List exposing (..)
 type alias Index = Int
 type alias Model = List Assessment.Model
 
-init : Model
-init =
-    [ { name = "Assignment 1", kind = Assignment, earned = 0, worth = 50 }
-    , { name = "Assignment 2", kind = Assignment, earned = 0, worth = 100 }
-    , { name = "Assignment 3", kind = Assignment, earned = 0, worth = 100 }
-    , { name = "Assignment 4", kind = Assignment, earned = 0, worth = 100 }
-    , { name = "Assignment 5", kind = Assignment, earned = 0, worth = 100 }
-    , { name = "Assignment 6", kind = Assignment, earned = 0, worth = 100 }
-    , { name = "Assignment 7", kind = Assignment, earned = 0, worth = 100 }
-    , { name = "Assignment 8", kind = Assignment, earned = 0, worth = 100 }
-    , { name = "Assignment 9", kind = Assignment, earned = 0, worth = 100 }
-
-    , { name = "Assignment 10", kind = Project, earned = 0, worth = 100 }
-
-    , { name = "Exam 1", kind = Exam, earned = 0, worth = 100 }
-    , { name = "Exam 2", kind = Exam, earned = 0, worth = 100 }
-    , { name = "Exam 3", kind = Exam, earned = 0, worth = 100 }
-
-    , { name = "Final", kind = Final, earned = 0, worth = 150 }
-    ]
-
 -- UPDATE
 
 type Action = Update Index Assessment.Action
